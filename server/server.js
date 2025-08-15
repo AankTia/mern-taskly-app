54be9caeff5c3b5924935caaeb11fa9a2a1ca40e
TiaWidi <tia.septianawidi@gmail.com>
2025-08-15 10:36:37 +0700

    Add api route

--- server/server.js
+++ server/server.js
@@ -3,6 +3,12 @@ import express from 'express';
 const app = express();
 const PORT = 8000;
 
+app.use('/api', (req, res) => {
+    res.status(200).json({
+        message: 'Hello, World!'
+    });
+});
+
 app.use((req, res) => {
     res.status(404).json({
         message: 'Not Found'

7544b27914c0695c7515af7886593e726297db76
TiaWidi <tia.septianawidi@gmail.com>
2025-08-15 13:08:00 +0700

    Update user routes

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { test } from '../controllers/user.controller.js';
+import { test, getUser, updateUser, deleteUser } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.get('/', test);
+router.get('/:id', getUser);
+router.patch('/update/:id', updateUser);
+router.delete('/delete/:id', deleteUser);
 
 export default router;

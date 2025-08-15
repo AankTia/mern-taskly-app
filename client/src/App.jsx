e71442b89ebe66eb07b0a382c406487068bb8858
TiaWidi <tia.septianawidi@gmail.com>
2025-08-15 14:50:21 +0700

    Adding React Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,17 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import SignIn from "../pages/SignIn";
+import SignUp from "../pages/SignUp";
 
-function App() {
-  const [visibility, setVisibility] = useState(true);
 
-  const toggleDiv = () => {
-    setVisibility(!visibility);
-  };
-
+export default function App() {
   return (
-    <div>
-      {visibility && (
-        <div id='myDiv'>
-          <p>Hello World!</p>
-        </div>
-      )}
-      <button onClick={toggleDiv}>Toogle Div</button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App

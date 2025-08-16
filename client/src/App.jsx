d1a7bbc81c89121031ca9ed837d97fd153d986d7
TiaWidi <tia.septianawidi@gmail.com>
2025-08-16 19:27:50 +0700

    The Context API

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,19 +4,22 @@ import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import { Toaster } from "react-hot-toast";
+import { UserProvider } from "./context/UserContext";
 
 
 export default function App() {
   return (
-    <ChakraProvider value={defaultSystem}>
-      <BrowserRouter>
-      <Toaster position="bottom-right" />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Routes>
-      </BrowserRouter>
-    </ChakraProvider>
+    <UserProvider>
+      <ChakraProvider value={defaultSystem}>
+        <BrowserRouter>
+          <Toaster position="bottom-right" />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        </BrowserRouter>
+      </ChakraProvider>
+    </UserProvider>
   );
 }

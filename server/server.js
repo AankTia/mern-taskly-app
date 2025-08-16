53a246c5cf890b6979a2eaf1db68272e13aa1ad3
TiaWidi <tia.septianawidi@gmail.com>
2025-08-16 19:10:24 +0700

    Enabling Cookie Parser Middleware

--- server/server.js
+++ server/server.js
@@ -3,11 +3,13 @@ import "dotenv/config";
 import userRoute from "./routes/user.route.js";
 import { errorHandler } from "./libs/middleware.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = 8000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('api/v1/auth', authRouter);
 app.use("/api/v1/users", userRoute);

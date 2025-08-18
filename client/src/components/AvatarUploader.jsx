5f3b0edb8dd1f370f94f2bbfada06ab4dd0b4a2d
TiaWidi <tia.septianawidi@gmail.com>
2025-08-18 15:34:44 +0700

    Refactor AvatarUploader

--- client/src/components/AvatarUploader.jsx
+++ client/src/components/AvatarUploader.jsx
@@ -1,38 +1,38 @@
-import { useCallback } from 'react';
-import { useDropzone } from 'react-dropzone';
-import { Center, Image, Tooltip, Input } from '@chakra-ui/react';
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+import { Center, Input, Image, Tooltip } from "@chakra-ui/react";
 
-export function AvatarUploader({ imageUrl, onFieldChange, setFile, setAvatar }) {
-  const convertFileToUrl = file => URL.createObjectURL(file);
+export function AvatarUploader({ imageUrl, onFieldChange, setFiles }) {
+  const convertFileToUrl = (file) => URL.createObjectURL(file);
 
-  const onDrop = useCallback(acceptedFiles => {
-    setFile(acceptedFiles);
+  const onDrop = useCallback((acceptedFiles) => {
+    setFiles(acceptedFiles);
     onFieldChange(convertFileToUrl(acceptedFiles[0]));
   }, []);
 
   const { getRootProps, getInputProps } = useDropzone({
     onDrop,
     accept: {
-      'image/jpeg':[],
-      'image/png': [],
+      "image/jpeg": [],
+      "image/png": [],
     },
   });
 
   return (
     <Center {...getRootProps()}>
-      <Input {...getInputProps()} id='avatar' cursor={'pointer'} />
+      <Input {...getInputProps()} id="avatar" cursor="pointer" />
       <Tooltip label="Change your avatar">
         <Image
-          alt='profile'
-          rounded='full'
-          h='24'
-          w='24'
-          objectFit='cover'
-          cursor='pointer'
-          mt='2'
+          alt="profile"
+          rounded="full"
+          h="24"
+          w="24"
+          objectFit="cover"
+          cursor="pointer"
+          mt="2"
           src={imageUrl}
         />
       </Tooltip>
     </Center>
-  )
+  );
 }

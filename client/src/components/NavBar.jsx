9e18ec12d7ce679e337ccb5d39cd11f186d2cd8c
TiaWidi <tia.septianawidi@gmail.com>
2025-08-18 10:59:06 +0700

    Refactor NavBar component: clean up import statements and improve code formatting; update API_BASE_URL definition in util.js

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,14 +2,14 @@ import { Link as RouterLink, useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import { useUser } from "../context/UserContext.jsx";
 import { API_BASE_URL } from "../util.js";
-import {
-  Box,
-  Flex,
-  Menu,
-  Spacer,
-  Link,
-  Button,
-  Image
+import { 
+  Box, 
+  Flex, 
+  Menu, 
+  Spacer, 
+  Link, 
+  Button, 
+  Image 
 } from "@chakra-ui/react";
 
 export default function NavBar() {
@@ -19,60 +19,35 @@ export default function NavBar() {
   const handleSignOut = async () => {
     try {
       const res = await fetch(`${API_BASE_URL}/auth/signout`, {
-        credentials: 'include',
+        credentials: "include",
       });
       const data = await res.json();
       toast.success(data.message);
       updateUser(null);
-      navigate('/');
+      navigate("/");
     } catch (error) {
       toast.error(error);
     }
   };
 
   return (
-    <Box as='nav' bg='red.50'>
+    <Box as="nav" bg="red.50">
       <Flex
-        minWidth='max-content'
-        alignItems='center'
-        p='12px'
-        maxW='7xl'
-        m='0 auto'
+        minWidth="max-content"
+        alignItems="center"
+        p="12px"
+        maxW="7xl"
+        m="0 auto"
       >
-        <Box p='2'>
-          <Link as={RouterLink} fontSize='lg' fontWeight='bold' to='/'>
+        <Box p="2">
+          <Link as={RouterLink} fontSize="lg" fontWeight="bold" to="/">
             Taskly
           </Link>
         </Box>
+
         <Spacer />
+
         <Box>
-          {user ? (
-            <Menu>
-              <Button>
-                <Image
-                  boxSize='40px'
-                  borderRadius='full'
-                  src={user.avatar}
-                  alt={user.username}
-                />
-              </Button>
-              <Menu.List>
-                <Menu.Item as={RouterLink} to='/profile'>
-                  Profile
-                </Menu.Item>
-                <Menu.Item as={RouterLink} to='/tasks'>
-                  Tasks
-                </Menu.Item>
-                <Menu.Item onClick={handleSignOut}>
-                  Sign Out
-                </Menu.Item>
-              </Menu.List>
-            </Menu>
-          ) : (
-            <Link as={RouterLink} to='/signin'>
-              Sign In
-            </Link>
-          )}
         </Box>
       </Flex>
     </Box>
--- client/src/util.js
+++ client/src/util.js
@@ -1,8 +1,5 @@
 import { useState, useEffect } from "react";
 
-export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api/v1";
-
 export const useLocalStorage = (key, defaultValue) => {
   const [value, setValue] = useState(() => {
     const currentValue = localStorage.getItem(key);
@@ -20,3 +17,5 @@ export const useLocalStorage = (key, defaultValue) => {
 
   return [value, setValue];
 };
+
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
